4c6e63705926f015d2381a80788f3619d060bbb1
Alexander Zagniotov <azagniotov@gmail.com>
2012-07-17 21:41:25 -0700

    1. Shell script

--- copy-artifact.sh
+++ copy-artifact.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-SOURCE="${BASH_SOURCE[0]}"
-echo "[INFO] Running "$SOURCE
+# SOURCE="${BASH_SOURCE[0]}"
+# echo "[INFO] Running "$SOURCE
 # Will work with spaces and other weird characters
 # Quote everything so that no strange characters can cause havoc
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-cp $DIR/target/stubby4j-1.0.1.jar $DIR/artifact/stubby4j-1.0.1.jar
-echo "[INFO] Copied $DIR/target/stubby4j-1.0.1.jar to $DIR/artifact/"
+# DIR="$( cd "$( dirname "$0" )" && pwd )"
+# cp $DIR/target/stubby4j-1.0.1.jar $DIR/artifact/stubby4j-1.0.1.jar
+# echo "[INFO] Copied $DIR/target/stubby4j-1.0.1.jar to $DIR/artifact/"
