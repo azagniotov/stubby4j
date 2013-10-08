11ca0122b64f0c1bbb013cba670cac078023f5c7
Alexander Zagniotov <azagniotov@gmail.com>
2013-10-08 11:36:49 -0700

    JS popup is not responsive to window resize events

--- main/resources/ui/js/main.js
+++ main/resources/ui/js/main.js
@@ -3,6 +3,7 @@ var $ = MINI.$, $$ = MINI.$$, EE = MINI.EE;
 var divFactory = EE('div');
 var anchorFactory = EE('a');
 var strongFactory = EE('strong');
+
 $(function () {
    $.ready(function () {
       bindLinks();
@@ -35,22 +36,19 @@ function ajaxClickHandler() {
 }
 
 function displayPopupWithContent(thisLink, parentTD, popupHtmlWithContent) {
+   var body = document.body;
+   var html = document.documentElement;
+
    var divRemovable = divFactory()[0];
    $(divRemovable).set({'@id': "popup-placeholder", 'innerHTML': popupHtmlWithContent});
    $('body').add($(divRemovable));
 
-   var body = document.body;
-   var html = document.documentElement;
-   var maskHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight, window.innerHeight);
-   var maskWidth = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth, window.innerWidth);
    var divPopupMask = 'div#popup-mask';
-   $(divPopupMask).set({$display: 'block', $opacity: '0.4', $width: maskWidth + 'px', $height: maskHeight + 'px'});
+   $(divPopupMask).set({$display: 'block', $opacity: '0.4', $width: getMaskWidth() + 'px', $height: getMaskHeight() + 'px'});
 
    var divPopupWindow = 'div#popup-window';
    $(divPopupWindow).set({$display: 'block'});
-   var topCoord = window.innerHeight / 2 - $$(divPopupWindow).offsetHeight / 2;
-   var leftCoord = window.innerWidth / 2 - $$(divPopupWindow).offsetWidth / 2;
-   $(divPopupWindow).set({$top: topCoord + "px", $left: leftCoord + "px"});
+   $(divPopupWindow).set({$top: getTopCoord() + "px", $left: getLeftCoord() + "px"});
 
    $(divPopupWindow + ' .close-dialog').on('click', function () {
       closePopupAndResetHandler();
@@ -68,6 +66,22 @@ function displayPopupWithContent(thisLink, parentTD, popupHtmlWithContent) {
       if (event.keyCode === 27) { //ESC key
          closePopupAndResetHandler();
       }
+   }
+
+   function getMaskHeight() {
+      return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight, window.innerHeight);
+   }
+
+   function getMaskWidth() {
+      return Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth, window.innerWidth);
+   }
+
+   function getTopCoord() {
+      return (window.innerHeight / 2 - $$(divPopupWindow).offsetHeight / 2);
+   }
+
+   function getLeftCoord() {
+      return (window.innerWidth / 2 - $$(divPopupWindow).offsetWidth / 2);
    }
 
    function closePopupAndResetHandler() {
@@ -93,5 +107,10 @@ function displayPopupWithContent(thisLink, parentTD, popupHtmlWithContent) {
       var strong = strongFactory()[0];
       parentTD.set('innerHTML', '');
       parentTD.add($(strong).add($(anchor)));
+   }
+
+   window.onresize = function(event) {
+      $(divPopupMask).set({$width: getMaskWidth() + 'px', $height: getMaskHeight() + 'px'});
+      $(divPopupWindow).set({$top: getTopCoord() + 'px', $left: getLeftCoord() + 'px'});
    }
 }
