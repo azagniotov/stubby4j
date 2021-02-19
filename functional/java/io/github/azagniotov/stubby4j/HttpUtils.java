f70570d981fce566003234de6c3348ab82bdf709
Alexander Zagniotov <azagniotov@gmail.com>
2021-02-19 22:32:28 +0900

    Plugged in ApacheHttpClient

--- functional/java/io/github/azagniotov/stubby4j/HttpUtils.java
+++ functional/java/io/github/azagniotov/stubby4j/HttpUtils.java
@@ -4,11 +4,15 @@ import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.apache.v2.ApacheHttpTransport;
 import io.github.azagniotov.stubby4j.utils.StringUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
 
 import java.io.IOException;
+import java.net.ProxySelector;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: Alexander Zagniotov
@@ -16,34 +20,46 @@ import java.io.IOException;
  */
 public final class HttpUtils {
 
-    private static final HttpRequestFactory WEB_CLIENT;
+   private static final HttpRequestFactory WEB_CLIENT;
 
-    static {
-        WEB_CLIENT = new NetHttpTransport().createRequestFactory(new HttpRequestInitializer() {
-            @Override
-            public void initialize(final HttpRequest request) {
-                request.setThrowExceptionOnExecuteError(false);
-                request.setReadTimeout(45000);
-                request.setConnectTimeout(45000);
-            }
-        });
-    }
+   static {
+      /**
+       * @see ApacheHttpTransport#newDefaultHttpClient()
+       */
+      final HttpClient apacheHttpClient = HttpClientBuilder.create()
+         .useSystemProperties()
+         //.setSSLSocketFactory(SSLConnectionSocketFactory.getSocketFactory())
+         .setSSLHostnameVerifier((hostname, session) -> true)
+         .setMaxConnTotal(200)
+         .setMaxConnPerRoute(20)
+         .setConnectionTimeToLive(-1, TimeUnit.MILLISECONDS)
+         .setRoutePlanner(new SystemDefaultRoutePlanner(ProxySelector.getDefault()))
+         .disableRedirectHandling()
+         .disableAutomaticRetries()
+         .build();
 
-    private HttpUtils() {
+      WEB_CLIENT = new ApacheHttpTransport(apacheHttpClient, false).createRequestFactory(request -> {
+         request.setThrowExceptionOnExecuteError(false);
+         request.setReadTimeout(45000);
+         request.setConnectTimeout(45000);
+      });
+   }
 
-    }
+   private HttpUtils() {
 
-    public static HttpRequest constructHttpRequest(final String method, final String targetUrl) throws IOException {
+   }
 
-        return WEB_CLIENT.buildRequest(method,
-                new GenericUrl(targetUrl),
-                null);
-    }
+   public static HttpRequest constructHttpRequest(final String method, final String targetUrl) throws IOException {
 
-    public static HttpRequest constructHttpRequest(final String method, final String targetUrl, final String content) throws IOException {
+      return WEB_CLIENT.buildRequest(method,
+         new GenericUrl(targetUrl),
+         null);
+   }
 
-        return WEB_CLIENT.buildRequest(method,
-                new GenericUrl(targetUrl),
-                new ByteArrayContent(null, StringUtils.getBytesUtf8(content)));
-    }
+   public static HttpRequest constructHttpRequest(final String method, final String targetUrl, final String content) throws IOException {
+
+      return WEB_CLIENT.buildRequest(method,
+         new GenericUrl(targetUrl),
+         new ByteArrayContent(null, StringUtils.getBytesUtf8(content)));
+   }
 }
