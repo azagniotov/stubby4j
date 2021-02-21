826643aca6fb8fcc1f40564c9c057ab77f807a3d
Alexander Zagniotov <azagniotov@gmail.com>
2021-02-21 16:53:38 +0900

    Formatted integration

--- integration/java/io/github/azagniotov/stubby4j/yaml/YamlParserLoadTest.java
+++ integration/java/io/github/azagniotov/stubby4j/yaml/YamlParserLoadTest.java
@@ -10,62 +10,62 @@ import static io.github.azagniotov.stubby4j.utils.FileUtils.BR;
 
 public class YamlParserLoadTest {
 
-    private static final YamlBuilder YAML_BUILDER = new YamlBuilder();
+   private static final YamlBuilder YAML_BUILDER = new YamlBuilder();
 
 
-    @Test
-    public void loadTest_shouldUnmarshallHugeYamlIntoObjectTree_WhenYAMLValid() throws Exception {
+   @Test
+   public void loadTest_shouldUnmarshallHugeYamlIntoObjectTree_WhenYAMLValid() throws Exception {
 
-        final String baseRequestUrl = "/some/uri";
+      final String baseRequestUrl = "/some/uri";
 
-        final String expectedHeaderKey = "location";
-        final String expectedHeaderValue = "/invoice/123";
+      final String expectedHeaderKey = "location";
+      final String expectedHeaderValue = "/invoice/123";
 
-        final String expectedParamOne = "paramOne";
-        final String expectedParamTwo = "paramTwo";
+      final String expectedParamOne = "paramOne";
+      final String expectedParamTwo = "paramTwo";
 
-        final String stubbedResponseBody = "Hello, this is a response body";
-        final String stubbedResponseStatus = "301";
+      final String stubbedResponseBody = "Hello, this is a response body";
+      final String stubbedResponseStatus = "301";
 
-        final int NUMBER_OF_HTTPCYCLES = 500;
-        final StringBuilder BUILDER = new StringBuilder(128);
+      final int NUMBER_OF_HTTPCYCLES = 500;
+      final StringBuilder BUILDER = new StringBuilder(128);
 
-        for (int idx = 1; idx <= NUMBER_OF_HTTPCYCLES; idx++) {
-            String expectedUrl = String.format("%s/%s", baseRequestUrl, idx);
+      for (int idx = 1; idx <= NUMBER_OF_HTTPCYCLES; idx++) {
+         String expectedUrl = String.format("%s/%s", baseRequestUrl, idx);
 
-            String yaml = YAML_BUILDER.newStubbedRequest()
-                    .withMethodGet()
-                    .withUrl(expectedUrl)
-                    .withQuery(expectedParamOne, String.valueOf(idx))
-                    .withQuery(expectedParamTwo, String.valueOf(idx))
-                    .newStubbedResponse()
-                    .withStatus(stubbedResponseStatus)
-                    .withHeaders(expectedHeaderKey, expectedHeaderValue)
-                    .withLiteralBody(stubbedResponseBody).build();
+         String yaml = YAML_BUILDER.newStubbedRequest()
+            .withMethodGet()
+            .withUrl(expectedUrl)
+            .withQuery(expectedParamOne, String.valueOf(idx))
+            .withQuery(expectedParamTwo, String.valueOf(idx))
+            .newStubbedResponse()
+            .withStatus(stubbedResponseStatus)
+            .withHeaders(expectedHeaderKey, expectedHeaderValue)
+            .withLiteralBody(stubbedResponseBody).build();
 
-            BUILDER.append(yaml).append(BR).append(BR);
-        }
+         BUILDER.append(yaml).append(BR).append(BR);
+      }
 
-        BUILDER.trimToSize();
-        final String rawYaml = BUILDER.toString();
-        BUILDER.setLength(0);
+      BUILDER.trimToSize();
+      final String rawYaml = BUILDER.toString();
+      BUILDER.setLength(0);
 
-        final YamlParseResultSet yamlParseResultSet = loadYamlToDataStore(rawYaml);
-        assertThat(yamlParseResultSet.getStubs().size()).isEqualTo(NUMBER_OF_HTTPCYCLES);
+      final YamlParseResultSet yamlParseResultSet = loadYamlToDataStore(rawYaml);
+      assertThat(yamlParseResultSet.getStubs().size()).isEqualTo(NUMBER_OF_HTTPCYCLES);
 
-        final StubHttpLifecycle actualHttpLifecycle = yamlParseResultSet.getStubs().get(498);
-        final StubRequest actualRequest = actualHttpLifecycle.getRequest();
-        final StubResponse actualResponse = actualHttpLifecycle.getResponse(true);
+      final StubHttpLifecycle actualHttpLifecycle = yamlParseResultSet.getStubs().get(498);
+      final StubRequest actualRequest = actualHttpLifecycle.getRequest();
+      final StubResponse actualResponse = actualHttpLifecycle.getResponse(true);
 
-        assertThat(actualRequest.getUrl()).contains(String.format("%s/%s", baseRequestUrl, 499));
-        assertThat(actualRequest.getUrl()).contains(String.format("%s=%s", expectedParamOne, 499));
-        assertThat(actualRequest.getUrl()).contains(String.format("%s=%s", expectedParamTwo, 499));
+      assertThat(actualRequest.getUrl()).contains(String.format("%s/%s", baseRequestUrl, 499));
+      assertThat(actualRequest.getUrl()).contains(String.format("%s=%s", expectedParamOne, 499));
+      assertThat(actualRequest.getUrl()).contains(String.format("%s=%s", expectedParamTwo, 499));
 
-        assertThat(actualResponse.getHeaders()).containsEntry(expectedHeaderKey, expectedHeaderValue);
-    }
+      assertThat(actualResponse.getHeaders()).containsEntry(expectedHeaderKey, expectedHeaderValue);
+   }
 
-    private YamlParseResultSet loadYamlToDataStore(final String yaml) throws Exception {
-        return new YamlParser().parse(".", yaml);
-    }
+   private YamlParseResultSet loadYamlToDataStore(final String yaml) throws Exception {
+      return new YamlParser().parse(".", yaml);
+   }
 
 }
