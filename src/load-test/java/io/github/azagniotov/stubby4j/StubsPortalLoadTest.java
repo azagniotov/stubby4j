75585d4c4cb7db3c6753b5e5d53e185c12e8dba6
Alexander Zagniotov <azagniotov@gmail.com>
2021-03-05 12:29:42 +0900

    Added ability to start stubby4j in load test by passing flags

--- src/load-test/java/io/github/azagniotov/stubby4j/StubsPortalLoadTest.java
+++ src/load-test/java/io/github/azagniotov/stubby4j/StubsPortalLoadTest.java
@@ -5,21 +5,26 @@ import com.google.api.client.http.HttpMethods;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import io.github.azagniotov.stubby4j.cli.ANSITerminal;
-import io.github.azagniotov.stubby4j.client.StubbyClient;
-import io.github.azagniotov.stubby4j.utils.StringUtils;
+import io.github.azagniotov.stubby4j.cli.CommandLineInterpreter;
+import io.github.azagniotov.stubby4j.server.JettyFactory;
+import io.github.azagniotov.stubby4j.server.StubbyManager;
+import io.github.azagniotov.stubby4j.server.StubbyManagerFactory;
 import io.github.azagniotov.stubby4j.yaml.YamlBuilder;
+import io.github.azagniotov.stubby4j.yaml.YamlParseResultSet;
+import io.github.azagniotov.stubby4j.yaml.YamlParser;
 import org.eclipse.jetty.http.HttpStatus;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.net.URL;
+import java.util.concurrent.CompletableFuture;
 
 import static com.google.common.truth.Truth.assertThat;
 import static io.github.azagniotov.stubby4j.common.Common.HEADER_APPLICATION_JSON;
@@ -31,13 +36,8 @@ public class StubsPortalLoadTest {
     private static final int ADMIN_PORT = PortTestUtils.findAvailableTcpPort();
 
     private static final String STUBS_URL = String.format("http://localhost:%s", STUBS_PORT);
-    private static final String ADMIN_URL = String.format("http://localhost:%s", ADMIN_PORT);
-    private static final String STUBS_SSL_URL = String.format("https://localhost:%s", STUBS_SSL_PORT);
-    private static final StubbyClient STUBBY_CLIENT = new StubbyClient();
-    private static String stubsData;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    private static StubbyManager stubbyManager;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -46,15 +46,36 @@ public class StubsPortalLoadTest {
 
         final URL url = StubsPortalLoadTest.class.getResource("/yaml/25k_stubs_load_test.yaml");
         final InputStream stubsDataInputStream = url.openStream();
-        stubsData = StringUtils.inputStreamToString(stubsDataInputStream);
         stubsDataInputStream.close();
 
-        STUBBY_CLIENT.startJetty(STUBS_PORT, STUBS_SSL_PORT, ADMIN_PORT, url.getFile());
+        final String[] args = new String[]{
+                "-m",
+                "-l", JettyFactory.DEFAULT_HOST,
+                "-s", String.valueOf(STUBS_PORT),
+                "-a", String.valueOf(ADMIN_PORT),
+                "-t", String.valueOf(STUBS_SSL_PORT),
+                //"--disable_stub_caching",
+        };
+
+        final CommandLineInterpreter commandLineInterpreter = new CommandLineInterpreter();
+        commandLineInterpreter.parseCommandLine(args);
+
+        final File configFile = new File(url.getFile());
+        final CompletableFuture<YamlParseResultSet> stubLoadComputation = CompletableFuture.supplyAsync(() -> {
+            try {
+                return new YamlParser().parse(configFile.getParent(), configFile);
+            } catch (IOException ioEx) {
+                throw new UncheckedIOException(ioEx);
+            }
+        });
+
+        stubbyManager = new StubbyManagerFactory().construct(configFile, commandLineInterpreter.getCommandlineParams(), stubLoadComputation);
+        stubbyManager.startJetty();
     }
 
     @AfterClass
     public static void afterClass() throws Exception {
-        STUBBY_CLIENT.stopJetty();
+        stubbyManager.stopJetty();
     }
 
     @Test
@@ -89,6 +110,8 @@ public class StubsPortalLoadTest {
     @Test
     public void cacheTesting_CheckResponseTimeOnRepeatedRequests() throws Exception {
 
+        // Set or unset flag: --disable_stub_caching in beforeClass()
+
         final int idx = 25_000;
 
         final String content = "{\"request_id\":\"abc_" + idx + "\", \"payload\":\"Yo, this is big!!\"}";
@@ -115,42 +138,29 @@ public class StubsPortalLoadTest {
         System.out.println(String.format("It took %s milliseconds to make a 1st request", firstElapsed));
         System.out.println("**************************************************************\n");
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // Making request#2
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        final HttpRequest requestTwo = HttpUtils.constructHttpRequest(HttpMethods.POST, requestUrl, content);
-        requestTwo.setHeaders(httpHeaders);
-
-        final long secondStart = System.currentTimeMillis();
-        final HttpResponse responseTwo = requestTwo.execute();
-        final long secondElapsed = System.currentTimeMillis() - secondStart;
-
-        final String responseTwoContentAsString = responseTwo.parseAsString().trim();
-
-        assertThat(responseTwo.getStatusCode()).isEqualTo(HttpStatus.CREATED_201);
-        assertThat("{\"status\":\"CREATED RESOURCE#" + idx + "!\"}").isEqualTo(responseTwoContentAsString);
-
-        System.out.println("\n**************************************************************");
-        System.out.println(String.format("It took %s milliseconds to make a 2nd request", secondElapsed));
-        System.out.println("**************************************************************");
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // Making request#3
+        // Making repeated requess
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        final HttpRequest requestThree = HttpUtils.constructHttpRequest(HttpMethods.POST, requestUrl, content);
-        requestThree.setHeaders(httpHeaders);
+        final int numberOfRepeatedRequests = 10;
+        for (int index = 2; index <= numberOfRepeatedRequests; index++) {
+            final HttpRequest requestTwo = HttpUtils.constructHttpRequest(HttpMethods.POST, requestUrl, content);
+            requestTwo.setHeaders(httpHeaders);
 
-        final long thirdStart = System.currentTimeMillis();
-        final HttpResponse responseThree = requestThree.execute();
-        final long thirdElapsed = System.currentTimeMillis() - thirdStart;
+            final long secondStart = System.currentTimeMillis();
+            final HttpResponse responseTwo = requestTwo.execute();
+            final long secondElapsed = System.currentTimeMillis() - secondStart;
 
-        final String responseThreeContentAsString = responseThree.parseAsString().trim();
+            final String responseTwoContentAsString = responseTwo.parseAsString().trim();
 
-        assertThat(responseThree.getStatusCode()).isEqualTo(HttpStatus.CREATED_201);
-        assertThat("{\"status\":\"CREATED RESOURCE#" + idx + "!\"}").isEqualTo(responseThreeContentAsString);
+            assertThat(responseTwo.getStatusCode()).isEqualTo(HttpStatus.CREATED_201);
+            assertThat("{\"status\":\"CREATED RESOURCE#" + idx + "!\"}").isEqualTo(responseTwoContentAsString);
 
-        System.out.println("**************************************************************");
-        System.out.println(String.format("It took %s milliseconds to make a 3rd request", thirdElapsed));
-        System.out.println("**************************************************************\n\n");
+            System.out.println("**************************************************************");
+            System.out.println(String.format("It took %s milliseconds to make a %s request", secondElapsed, index));
+            System.out.println("**************************************************************");
+        }
+
+        System.out.println("\n\n");
     }
 }
