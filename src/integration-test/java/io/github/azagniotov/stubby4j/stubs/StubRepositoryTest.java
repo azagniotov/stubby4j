bf0235050fb7416f511a48bdeb5ca3d6281d0859
Alexander Zagniotov <azagniotov@gmail.com>
2021-03-16 18:27:37 +0900

    Added integration test to proxy request by stub repository

--- src/integration-test/java/io/github/azagniotov/stubby4j/stubs/StubRepositoryTest.java
+++ src/integration-test/java/io/github/azagniotov/stubby4j/stubs/StubRepositoryTest.java
@@ -1,6 +1,7 @@
 package io.github.azagniotov.stubby4j.stubs;
 
 import com.google.api.client.http.HttpMethods;
+import io.github.azagniotov.stubby4j.annotations.PotentiallyFlaky;
 import io.github.azagniotov.stubby4j.caching.Cache;
 import io.github.azagniotov.stubby4j.common.Common;
 import io.github.azagniotov.stubby4j.utils.FileUtils;
@@ -34,6 +35,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static io.github.azagniotov.stubby4j.stubs.StubbableAuthorizationType.BASIC;
 import static io.github.azagniotov.stubby4j.stubs.StubbableAuthorizationType.BEARER;
 import static io.github.azagniotov.stubby4j.stubs.StubbableAuthorizationType.CUSTOM;
+import static io.github.azagniotov.stubby4j.utils.FileUtils.BR;
 import static io.github.azagniotov.stubby4j.utils.ReflectionUtils.injectObjectFields;
 import static io.github.azagniotov.stubby4j.utils.StringUtils.inputStreamToString;
 import static org.mockito.ArgumentMatchers.any;
@@ -1097,6 +1099,50 @@ public class StubRepositoryTest {
                 assertThat(sequenceStubResponse.getHeaders().get(Common.HEADER_X_STUBBY_RESOURCE_ID)).isEqualTo(String.valueOf(resourceId));
             }
         }
+    }
+
+    @Test
+    @PotentiallyFlaky("This test sending the request over the wire to https://jsonplaceholder.typicode.com")
+    public void shouldReturnProxiedRequestResponse_WhenStubsWereNotMatched() throws Exception {
+
+        // https://jsonplaceholder.typicode.com/todos/1
+        final String targetUriPath = "/todos/1";
+
+        final String stubsYaml = YAML_BUILDER.newStubbedRequest()
+                .withMethodGet()
+                .withUrl("/a/totally/different/endpoint/stubbed")
+                .newStubbedResponse()
+                .withStatus("200")
+                .withLiteralBody("This is a response for todo 1")
+                .build();
+
+        final String proxyConfigYaml = YAML_BUILDER
+                .newStubbedProxyConfig()
+                .witProxyStrategyAsIs()
+                .withProxyPropertyEndpoint("https://jsonplaceholder.typicode.com")
+                .toString()
+                .trim();
+
+        loadYamlToDataStore(stubsYaml + BR + BR + proxyConfigYaml);
+
+        final StubRequest assertingRequest =
+                requestBuilder
+                        .withUrl(targetUriPath)
+                        .withMethodGet().build();
+
+        final StubResponse foundStubResponse = setUpStubSearchBehavior(assertingRequest);
+
+        assertThat(Code.USE_PROXY).isEqualTo(foundStubResponse.getHttpStatusCode());
+        assertThat(foundStubResponse.getHeaders().containsKey("Server")).isTrue();
+        assertThat(foundStubResponse.getHeaders().containsKey("x-ratelimit-remaining")).isTrue();
+
+        assertThat(foundStubResponse.getBody()).isEqualTo(
+                "{" + BR +
+                        "  \"userId\": 1," + BR +
+                        "  \"id\": 1," + BR +
+                        "  \"title\": \"delectus aut autem\"," + BR +
+                        "  \"completed\": false" + BR +
+                        "}");
     }
 
     private void loadYamlToDataStore(final String yaml) throws Exception {
