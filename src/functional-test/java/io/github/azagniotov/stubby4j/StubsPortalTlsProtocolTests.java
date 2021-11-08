36a372dba06ab9de9be0fc8a70ad56129eb76d6d
Alexander Zagniotov <azagniotov@gmail.com>
2021-11-08 20:26:56 +0900

    sfdsd

--- src/functional-test/java/io/github/azagniotov/stubby4j/StubsPortalTlsProtocolTests.java
+++ src/functional-test/java/io/github/azagniotov/stubby4j/StubsPortalTlsProtocolTests.java
@@ -36,7 +36,6 @@ import javax.net.ssl.SSLEngine;
 import java.io.InputStream;
 import java.net.ProxySelector;
 import java.net.URL;
-import java.security.Security;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -217,6 +216,50 @@ public class StubsPortalTlsProtocolTests {
 
         supportedCipherSuites.addAll(
                 Arrays.asList(
+
+                        "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384",
+                        "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_DHE_DSS_WITH_AES_128_GCM_SHA256",
+                        "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384",
+                        "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
+                        "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256",
+                        "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256",
+                        "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
+                        "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256",
+                        "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256",
+                        "TLS_DHE_DSS_WITH_AES_128_CBC_SHA256",
+                        "TLS_ECDH_ECDSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_ECDH_RSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_ECDH_ECDSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA384",
+                        "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA384",
+                        "TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA256",
+                        "TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256",
+                        "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
+                        "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
+                        "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA",
+                        "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+                        "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+                        "TLS_DHE_DSS_WITH_AES_256_CBC_SHA",
+                        "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+                        "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
+                        "TLS_ECDH_ECDSA_WITH_AES_256_CBC_SHA",
+                        "TLS_ECDH_RSA_WITH_AES_256_CBC_SHA",
+                        "TLS_ECDH_ECDSA_WITH_AES_128_CBC_SHA",
+                        "TLS_ECDH_RSA_WITH_AES_128_CBC_SHA",
+                        "TLS_RSA_WITH_AES_256_GCM_SHA384",
+                        "TLS_RSA_WITH_AES_128_GCM_SHA256",
+                        "TLS_RSA_WITH_AES_256_CBC_SHA256",
+                        "TLS_RSA_WITH_AES_128_CBC_SHA256",
+                        "TLS_RSA_WITH_AES_256_CBC_SHA",
+                        "TLS_RSA_WITH_AES_128_CBC_SHA",
+                        "TLS_EMPTY_RENEGOTIATION_INFO_SCSV",
 
                         // *_CHACHA20_POLY1305 are 3x to 4x faster than existing cipher suites.
                         //   http://googleonlinesecurity.blogspot.com/2014/04/speeding-up-and-strengthening-https.html
--- src/main/java/io/github/azagniotov/stubby4j/Main.java
+++ src/main/java/io/github/azagniotov/stubby4j/Main.java
@@ -31,11 +31,6 @@ import static io.github.azagniotov.stubby4j.utils.FileUtils.BR;
 @GeneratedCodeCoverageExclusion
 public final class Main {
 
-    static {
-        Security.setProperty("jdk.tls.disabledAlgorithms", "SSLv3, RC4, DH keySize < 1024, EC keySize < 224, DES40_CBC, RC4_40, 3DES_EDE_CBC");
-        Security.setProperty("jdk.certpath.disabledAlgorithms", "MD2, SHA1 jdkCA & usage TLSServer, RSA keySize < 1024, DSA keySize < 1024, EC keySize < 224");
-    }
-
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     private static final String DEFAULT_CONFIG_FILE = "/yaml/empty-stub.yaml";
