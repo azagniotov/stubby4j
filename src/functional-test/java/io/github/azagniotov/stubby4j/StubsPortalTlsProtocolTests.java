15eeb3a62a7c8dd343fa405abadca0505a555df7
Alexander Zagniotov <azagniotov@gmail.com>
2021-11-11 17:30:06 +0900

    StubsPortalTlsProtocolTests cleanup
    Using Apache client directly

--- src/functional-test/java/io/github/azagniotov/stubby4j/StubsPortalTlsProtocolTests.java
+++ src/functional-test/java/io/github/azagniotov/stubby4j/StubsPortalTlsProtocolTests.java
@@ -1,15 +1,13 @@
 package io.github.azagniotov.stubby4j;
 
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpMethods;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.apache.v2.ApacheHttpTransport;
 import io.github.azagniotov.stubby4j.cli.ANSITerminal;
 import io.github.azagniotov.stubby4j.client.StubbyClient;
 import io.github.azagniotov.stubby4j.client.StubbyResponse;
 import io.github.azagniotov.stubby4j.utils.StringUtils;
-import org.apache.http.client.HttpClient;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
@@ -17,21 +15,22 @@ import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
 import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
 import org.eclipse.jetty.http.HttpStatus;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.ProxySelector;
 import java.net.URL;
@@ -41,6 +40,7 @@ import java.util.concurrent.TimeUnit;
 
 import static com.google.common.truth.Truth.assertThat;
 import static io.github.azagniotov.stubby4j.common.Common.HEADER_APPLICATION_JSON;
+import static io.github.azagniotov.stubby4j.server.ssl.SslUtils.SSLv3;
 import static io.github.azagniotov.stubby4j.server.ssl.SslUtils.TLS_v1;
 import static io.github.azagniotov.stubby4j.server.ssl.SslUtils.TLS_v1_1;
 import static io.github.azagniotov.stubby4j.server.ssl.SslUtils.TLS_v1_2;
@@ -48,16 +48,18 @@ import static io.github.azagniotov.stubby4j.server.ssl.SslUtils.TLS_v1_3;
 
 public class StubsPortalTlsProtocolTests {
 
+    private static final StubbyClient STUBBY_CLIENT = new StubbyClient();
     private static final int STUBS_PORT = PortTestUtils.findAvailableTcpPort();
     private static final int STUBS_SSL_PORT = PortTestUtils.findAvailableTcpPort();
     private static final int ADMIN_PORT = PortTestUtils.findAvailableTcpPort();
+
     private static final String ADMIN_URL = String.format("http://localhost:%s", ADMIN_PORT);
     private static final String STUBS_SSL_URL = String.format("https://localhost:%s", STUBS_SSL_PORT);
-    private static final StubbyClient STUBBY_CLIENT = new StubbyClient();
-    private static String stubsData;
 
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    private static final String REQUEST_URL = String.format("%s%s", STUBS_SSL_URL, "/invoice?status=active&type=full");
+
+    private static String stubsData;
+    private static String expectedContent;
 
     @BeforeClass
     public static void beforeClass() throws Exception {
@@ -65,11 +67,17 @@ public class StubsPortalTlsProtocolTests {
         ANSITerminal.muteConsole(true);
 
         final URL url = StubsPortalTest.class.getResource("/yaml/main-test-stubs.yaml");
+        assert url != null;
+
         final InputStream stubsDataInputStream = url.openStream();
         stubsData = StringUtils.inputStreamToString(stubsDataInputStream);
         stubsDataInputStream.close();
 
         STUBBY_CLIENT.startJetty(STUBS_PORT, STUBS_SSL_PORT, ADMIN_PORT, url.getFile());
+
+        final URL jsonContentUrl = StubsPortalTlsProtocolTests.class.getResource("/json/response/json_response_1.json");
+        assertThat(jsonContentUrl).isNotNull();
+        expectedContent = StringUtils.inputStreamToString(jsonContentUrl.openStream());
     }
 
     @AfterClass
@@ -84,86 +92,36 @@ public class StubsPortalTlsProtocolTests {
     }
 
     @After
-    public void afterEach() throws Exception {
+    public void afterEach() {
         ANSITerminal.muteConsole(true);
     }
 
     @Test
+    public void shouldReturnExpectedResponseWhenGetRequestMadeOverSslWithSslVersion_SSLv3() throws Exception {
+        makeRequestAndAssert(buildHttpClient(SSLv3));
+    }
+
+    @Test
     public void shouldReturnExpectedResponseWhenGetRequestMadeOverSslWithTlsVersion_1_0() throws Exception {
-
-        final URL jsonContentUrl = StubsPortalTest.class.getResource("/json/response/json_response_1.json");
-        assertThat(jsonContentUrl).isNotNull();
-        final String expectedContent = StringUtils.inputStreamToString(jsonContentUrl.openStream());
-
-        final String requestUrl = String.format("%s%s", STUBS_SSL_URL, "/invoice?status=active&type=full");
-        final HttpRequestFactory httpClient = buildHttpClient(TLS_v1);
-        final HttpResponse response = httpClient.buildRequest(HttpMethods.GET, new GenericUrl(requestUrl), null).execute();
-
-        final String contentTypeHeader = response.getContentType();
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK_200);
-        assertThat(expectedContent).isEqualTo(response.parseAsString().trim());
-        assertThat(contentTypeHeader).contains(HEADER_APPLICATION_JSON);
+        makeRequestAndAssert(buildHttpClient(TLS_v1));
     }
 
     @Test
     public void shouldReturnExpectedResponseWhenGetRequestMadeOverSslWithTlsVersion_1_1() throws Exception {
-
-        final URL jsonContentUrl = StubsPortalTest.class.getResource("/json/response/json_response_1.json");
-        assertThat(jsonContentUrl).isNotNull();
-        final String expectedContent = StringUtils.inputStreamToString(jsonContentUrl.openStream());
-
-        final String requestUrl = String.format("%s%s", STUBS_SSL_URL, "/invoice?status=active&type=full");
-        final HttpRequestFactory httpClient = buildHttpClient(TLS_v1_1);
-        final HttpResponse response = httpClient.buildRequest(HttpMethods.GET, new GenericUrl(requestUrl), null).execute();
-
-        final String contentTypeHeader = response.getContentType();
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK_200);
-        assertThat(expectedContent).isEqualTo(response.parseAsString().trim());
-        assertThat(contentTypeHeader).contains(HEADER_APPLICATION_JSON);
+        makeRequestAndAssert(buildHttpClient(TLS_v1_1));
     }
 
     @Test
     public void shouldReturnExpectedResponseWhenGetRequestMadeOverSslWithTlsVersion_1_2() throws Exception {
-
-        final URL jsonContentUrl = StubsPortalTest.class.getResource("/json/response/json_response_1.json");
-        assertThat(jsonContentUrl).isNotNull();
-        final String expectedContent = StringUtils.inputStreamToString(jsonContentUrl.openStream());
-
-        final String requestUrl = String.format("%s%s", STUBS_SSL_URL, "/invoice?status=active&type=full");
-        final HttpRequestFactory httpClient = buildHttpClient(TLS_v1_2);
-        final HttpResponse response = httpClient.buildRequest(HttpMethods.GET, new GenericUrl(requestUrl), null).execute();
-
-        final String contentTypeHeader = response.getContentType();
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK_200);
-        assertThat(expectedContent).isEqualTo(response.parseAsString().trim());
-        assertThat(contentTypeHeader).contains(HEADER_APPLICATION_JSON);
+        makeRequestAndAssert(buildHttpClient(TLS_v1_2));
     }
 
     @Test
     public void shouldReturnExpectedResponseWhenGetRequestMadeOverSslWithTlsVersion_1_3() throws Exception {
-
-        final URL jsonContentUrl = StubsPortalTest.class.getResource("/json/response/json_response_1.json");
-        assertThat(jsonContentUrl).isNotNull();
-        final String expectedContent = StringUtils.inputStreamToString(jsonContentUrl.openStream());
-
-        final String requestUrl = String.format("%s%s", STUBS_SSL_URL, "/invoice?status=active&type=full");
-        final HttpRequestFactory httpClient = buildHttpClient(TLS_v1_3);
-        final HttpResponse response = httpClient.buildRequest(HttpMethods.GET, new GenericUrl(requestUrl), null).execute();
-
-        final String contentTypeHeader = response.getContentType();
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK_200);
-        assertThat(expectedContent).isEqualTo(response.parseAsString().trim());
-        assertThat(contentTypeHeader).contains(HEADER_APPLICATION_JSON);
+        makeRequestAndAssert(buildHttpClient(TLS_v1_3));
     }
 
-    /**
-     * @see ApacheHttpTransport#newDefaultHttpClient()
-     */
-    private HttpRequestFactory buildHttpClient(final String tlsVersion) throws Exception {
+    private CloseableHttpClient buildHttpClient(final String tlsVersion) throws Exception {
 
         System.out.println("Running tests using TLS version: " + tlsVersion);
 
@@ -191,7 +149,12 @@ public class StubsPortalTlsProtocolTests {
                         .register("http", new PlainConnectionSocketFactory())
                         .build();
 
-        final HttpClient apacheHttpClient = HttpClientBuilder.create()
+        return HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig
+                        .custom()
+                        .setSocketTimeout(45000)
+                        .setConnectTimeout(45000)
+                        .build())
                 .setSSLContext(sslContext)
                 .setSSLSocketFactory(sslSocketFactory)
                 .setConnectionManager(new BasicHttpClientConnectionManager(socketFactoryRegistry))
@@ -208,11 +171,16 @@ public class StubsPortalTlsProtocolTests {
                 .disableContentCompression()
                 .disableAutomaticRetries()
                 .build();
+    }
 
-        return new ApacheHttpTransport(apacheHttpClient, false).createRequestFactory(request -> {
-            request.setThrowExceptionOnExecuteError(false);
-            request.setReadTimeout(45000);
-            request.setConnectTimeout(45000);
-        });
+    private void makeRequestAndAssert(CloseableHttpClient httpClient) throws IOException {
+        try (final CloseableHttpResponse response = httpClient.execute(new HttpGet(REQUEST_URL))) {
+
+            final HttpEntity responseEntity = response.getEntity();
+
+            assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.OK_200);
+            assertThat(expectedContent).isEqualTo(EntityUtils.toString(responseEntity));
+            assertThat(responseEntity.getContentType().getValue()).contains(HEADER_APPLICATION_JSON);
+        }
     }
 }
