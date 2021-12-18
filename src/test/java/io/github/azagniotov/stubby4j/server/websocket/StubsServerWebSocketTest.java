c76dbf89eaa11aaf612054bfe357f1394517ba31
Alexander Zagniotov <azagniotov@gmail.com>
2021-12-19 00:46:38 +0900

    Added Mockito inline dependency

--- src/test/java/io/github/azagniotov/stubby4j/server/websocket/StubsServerWebSocketTest.java
+++ src/test/java/io/github/azagniotov/stubby4j/server/websocket/StubsServerWebSocketTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.nio.ByteBuffer;
@@ -27,7 +28,6 @@ import java.util.concurrent.TimeUnit;
 import static com.google.common.truth.Truth.assertThat;
 import static io.github.azagniotov.stubby4j.server.websocket.StubsServerWebSocket.EMPTY_BYTE_BUFFER;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,13 +51,14 @@ public class StubsServerWebSocketTest {
     @Captor
     private ArgumentCaptor<Runnable> runnableCaptor;
 
-    private ScheduledExecutorService spyScheduledExecutorService;
+    @Spy
+    // https://github.com/mockito/mockito/issues/2231
+    private ScheduledExecutorService spyScheduledExecutorService = Executors.newScheduledThreadPool(10);
 
     private StubsServerWebSocket serverWebSocket;
 
     @Before
     public void setUp() throws Exception {
-        spyScheduledExecutorService = spy(Executors.newScheduledThreadPool(10));
         when(mockSession.getRemote()).thenReturn(mockRemoteEndpoint);
     }
 
