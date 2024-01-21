7de0f245b1232150c03dd103dd653874befa5e58
Alexander Zagniotov <azagniotov@gmail.com>
2024-01-21 01:25:07 -0800

    Updated SnakeYaml constructor

--- src/main/java/io/github/azagniotov/stubby4j/yaml/SnakeYaml.java
+++ src/main/java/io/github/azagniotov/stubby4j/yaml/SnakeYaml.java
@@ -1,6 +1,7 @@
 package io.github.azagniotov.stubby4j.yaml;
 
 import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.representer.Representer;
@@ -13,7 +14,7 @@ public enum SnakeYaml {
     private final Yaml snakeYaml;
 
     SnakeYaml() {
-        snakeYaml = new Yaml(new Constructor(), new Representer(), new DumperOptions(), new YamlParserResolver());
+        snakeYaml = new Yaml(new Constructor(new LoaderOptions()), new Representer(new DumperOptions()), new DumperOptions(), new YamlParserResolver());
     }
 
     public Yaml getSnakeYaml() {
