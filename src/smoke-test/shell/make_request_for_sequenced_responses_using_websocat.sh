9e59ed71166df06b7ad05b8a1dcb57463315f919
Alexander Zagniotov <azagniotov@gmail.com>
2024-02-05 13:51:36 -0800

    Update make_request_for_sequenced_responses_using_websocat.sh

--- src/smoke-test/shell/make_request_for_sequenced_responses_using_websocat.sh
+++ src/smoke-test/shell/make_request_for_sequenced_responses_using_websocat.sh
@@ -25,8 +25,7 @@ then
   echo "WebSocket request to $2:$3 failed, exiting with status 1 ... "
   exit 1
 else
-  echo "$smoke_test_connection_response"
-  exit 0
+  echo "Got success status: $smoke_test_connection_response"
 fi
 
 websocat --text --exit-on-eof --protocol "zumba" -q -uU tcp-l:127.0.0.1:1234 reuse-raw:$1://$2:$3/ws/hello-world/2 --max-messages-rev 1&
@@ -49,8 +48,7 @@ then
   echo "WebSocket request to $2:$3 failed, got: $smoke_test_sequenced_response ... Exiting with status 1 ... "
   exit 1
 else
-  echo "$smoke_test_sequenced_response"
-  exit 0
+  echo "Got sequenced response: $smoke_test_sequenced_response"
 fi
 
 echo ""
@@ -64,6 +62,19 @@ then
   echo "WebSocket request to $2:$3 failed, got: $smoke_test_sequenced_response ... Exiting with status 1 ... "
   exit 1
 else
-  echo "$smoke_test_sequenced_response"
-  exit 0
+  echo "Got sequenced response: $smoke_test_sequenced_response"
+fi
+
+echo ""
+echo "#####################################################################"
+echo "## Got $smoke_test_sequenced_response" 
+echo "#####################################################################"
+
+smoke_test_sequenced_response=$(echo "Hello, World!" | nc 127.0.0.1 1234 2> /dev/null; echo  $?)
+if [ "$smoke_test_sequenced_response" != "world-1567" ]
+then
+  echo "WebSocket request to $2:$3 failed, got: $smoke_test_sequenced_response ... Exiting with status 1 ... "
+  exit 1
+else
+  echo "Got sequenced response: $smoke_test_sequenced_response"
 fi
