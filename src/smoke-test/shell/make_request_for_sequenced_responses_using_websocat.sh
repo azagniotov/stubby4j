1c9746e2748dfd3a28636b401ae757de55cc056e
Alexander Zagniotov <azagniotov@gmail.com>
2024-02-05 16:21:08 -0800

    Update make_request_for_sequenced_responses_using_websocat.sh

--- src/smoke-test/shell/make_request_for_sequenced_responses_using_websocat.sh
+++ src/smoke-test/shell/make_request_for_sequenced_responses_using_websocat.sh
@@ -28,59 +28,38 @@ else
   echo "Got success status: $smoke_test_connection_response"
 fi
 
-echo "Hello, World!" | websocat -q -uU --protocol "zumba" $1://$2:$3/ws/hello-world/2
 
-websocat --text --exit-on-eof --protocol "zumba" -q -uU tcp-l:127.0.0.1:1234 reuse-raw:$1://$2:$3/ws/hello-world/2 --max-messages-rev 1&
-echo "Hello, World!" | nc 127.0.0.1 1234
-echo "Hello, World!" | nc 127.0.0.1 1234
-echo "Hello, World!" | nc 127.0.0.1 1234
-echo "Hello, World!" | nc 127.0.0.1 1234
-
+for idx in $(seq 0 4); do echo "Hello, World!"; sleep 1; done | websocat --protocol "zumba" $1://$2:$3/ws/hello-world/2 > sequenced_responses.txt
 echo ""
 echo "#####################################################################"
-echo "## WebSoCat is TCP listening on 127.0.0.1:1234" 
+echo "## WebSocket sequenced responses received from $1://$2:$3/ws/hello-world/2"
 echo "#####################################################################"
 
-
-smoke_test_sequenced_response=$(echo "Hello, World!" | nc 127.0.0.1 1234)
-echo ""
-echo "#####################################################################"
-echo "## Got $smoke_test_sequenced_response" 
-echo "#####################################################################"
-
-
-if [ "$smoke_test_sequenced_response" != "world-0" ]
-then
-  echo "WebSocket request to $2:$3 failed, got: $smoke_test_sequenced_response ... Exiting with status 1 ... "
-  exit 1
-else
-  echo "Got sequenced response: $smoke_test_sequenced_response"
+if grep -xq "world-0" "sequenced_responses.txt"; then 
+    echo "Received 'world-0' from the WebSocket server"
+else 
+    echo "WebSocket response from $2:$3 is incomplete, could not receive 'world-0' ... Exiting with status 1 ... "
+    exit 1
 fi
 
-echo ""
-echo "#####################################################################"
-echo "## Got $smoke_test_sequenced_response" 
-echo "#####################################################################"
-
-smoke_test_sequenced_response=$(echo "Hello, World!" | nc 127.0.0.1 1234 2> /dev/null; echo  $?)
-if [ "$smoke_test_sequenced_response" != "world-1" ]
-then
-  echo "WebSocket request to $2:$3 failed, got: $smoke_test_sequenced_response ... Exiting with status 1 ... "
-  exit 1
-else
-  echo "Got sequenced response: $smoke_test_sequenced_response"
+if grep -xq "world-1" "sequenced_responses.txt"; then 
+    echo "Received 'world-1' from the WebSocket server"
+else 
+    echo "WebSocket response from $2:$3 is incomplete, could not receive 'world-1' ... Exiting with status 1 ... "
+    exit 1
 fi
 
-echo ""
-echo "#####################################################################"
-echo "## Got $smoke_test_sequenced_response" 
-echo "#####################################################################"
-
-smoke_test_sequenced_response=$(echo "Hello, World!" | nc 127.0.0.1 1234 2> /dev/null; echo  $?)
-if [ "$smoke_test_sequenced_response" != "world-1567" ]
-then
-  echo "WebSocket request to $2:$3 failed, got: $smoke_test_sequenced_response ... Exiting with status 1 ... "
-  exit 1
-else
-  echo "Got sequenced response: $smoke_test_sequenced_response"
+if grep -xq "world-2a,world-2b,world-2c,world-2d,world-2e" "sequenced_responses.txt"; then 
+    echo "Received 'world-2a,world-2b,world-2c,world-2d,world-2e' from the WebSocket server"
+else 
+    echo "WebSocket response from $2:$3 is incomplete, could not receive 'world-2a,world-2b,world-2c,world-2d,world-2e' ... Exiting with status 1 ... "
+    exit 1
 fi
+
+if grep -xq "world-3" "sequenced_responses.txt"; then 
+    echo "Received 'world-3' from the WebSocket server"
+else 
+    echo "WebSocket response from $2:$3 is incomplete, could not receive 'world-3' ... Exiting with status 1 ... "
+    exit 1
+fi
+
